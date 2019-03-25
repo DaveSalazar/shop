0aca0b9a597a58f86557c56cee415b0b96bcf886
Dave Salazar <salazardave93@gmail.com>
2019-03-25 14:48:56 -0500

    implement repository pattern

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -6,6 +6,8 @@
 	{
 		public int Id { get; set; }
 
+		[MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+		[Required]
 		public string Name { get; set; }
 
 		[DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -15,10 +17,10 @@
 		public string ImageUrl { get; set; }
 
 		[Display(Name = "Last Purchase")]
-		public DateTime LastPurchase { get; set; }
+		public DateTime? LastPurchase { get; set; }
 
 		[Display(Name = "Last Sale")]
-		public DateTime LastSale { get; set; }
+		public DateTime? LastSale { get; set; }
 
 		[Display(Name = "Is Availabe?")]
 		public bool IsAvailabe { get; set; }
--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -30,7 +30,8 @@ namespace Shop.Web
 			{
 				cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
 			});
-
+			services.AddTransient<SeedDb>();
+			services.AddScoped<IRepository, Repository>();
 			services.Configure<CookiePolicyOptions>(options =>
 			{
 				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
